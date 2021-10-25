478c0f134df478ba59dea5a80e57b7fc7c8142a4
Allen B <pingguo.ny@gmail.com>
2021-10-24 20:41:09 -0400

    switch benchmark

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -17,11 +17,16 @@
 
 #define NS_PER_S 1000000000
 
-#define B_START clock_t t = clock(); size_t mem = (size_t)mallinfo().uordblks
-#define B_END t = clock() - t; mem = (size_t)mallinfo().uordblks - mem
-#define B_PRINT(type, op) printf(type "," op ",%d,%.2f,%llu\n", n, ((double)t) / CLOCKS_PER_SEC * NS_PER_S / n, (unsigned long long)(mem > INT_MAX ? 0 : mem))
+#define B_TIME(type, op, block) do {\
+    size_t mem = (size_t)mallinfo().uordblks; clock_t t = clock(); \
+    block; \
+    t = clock() - t; mem = (size_t)mallinfo().uordblks - mem; \
+    printf(#type "," op ",%d,%.2f,%llu\n", n, ((double)t) / CLOCKS_PER_SEC * NS_PER_S / n, (unsigned long long)mem); \
+} while(0)
 
 #define INT_TO_PTR(i) (void*)(uintptr_t)(i)
+
+#include <stdlib.h>
 
 int n_values[] = {1 << 6, 1 << 7, 1 << 8, 1 << 9, (1<<9) + (1<<8), 1 << 10,
     1 << 14, 1 << 17, 1 << 18, 1 << 19, 1 << 20};
@@ -29,52 +34,43 @@ static char* strings[1 << 22];
 void setup_strings() {
     for (int i = 0; i < sizeof(strings) / sizeof(strings[0]); i++) {
         strings[i] = malloc(16);
-        sprintf(strings[i], "%d", i);
+        sprintf(strings[i], "%x", (unsigned)rand());
     }
 }
-
 
 void benchmark_kvec(int n) {
     kvec_t(int) arr;
     kv_init(arr);
 
-    B_START;
-    for (int i = 0; i < n; i++) {
-        kv_push(int, arr, 1);
-    }
-    B_END;
+    B_TIME(kvec_t, "append", {
+        for (int i = 0; i < n; i++) {
+            kv_push(int, arr, 1);
+        }
+    });
 
     kv_destroy(arr);
-
-    B_PRINT("kvec_t", "add");
 }
 
 void benchmark_arrlist(int n) {
     ArrList arr;
     arrlist_init(&arr, 0);
 
-    B_START;
-
-    for (int i = 0; i < n; i++) {
-        arrlist_add(&arr, INT_TO_PTR(i));
-    }
-
-    B_END;
+    B_TIME(ArrList, "append", {
+        for (int i =0 ; i<n;i++)
+            arrlist_add(&arr, INT_TO_PTR(i));
+    });
 
     arrlist_deinit(&arr);
-
-    B_PRINT("ArrList", "add");
 }
 
 void benchmark_garray(int n) {
     GArray* arr = g_array_new(FALSE, FALSE, sizeof(int));
 
-    B_START;
-    for (int i = 0; i < n; i++) {
-        g_array_append_vals(arr, &i, 1);
-    }
-    B_END;
-    B_PRINT("GArray", "add");
+    B_TIME(GArray, "append", {
+        for (int i = 0; i < n; i++) {
+            g_array_append_vals(arr, &i, 1);
+        }
+    });
 
     g_array_free(arr, TRUE);
 }
@@ -86,22 +82,23 @@ void benchmark_rhmap(int n) {
         return;
     }
 
-    {
-        B_START;
+    B_TIME(RhMap, "ins", {
         for (int i = 0; i < n; i++) {
             rhmap_set(&map, strings[i], INT_TO_PTR(i));
         }
-        B_END;
-        B_PRINT("RhMap", "ins");
-    }
-    {
-        B_START;
+    });
+
+    B_TIME(RhMap, "get", {
+        for (int i = 0; i < n; i++) {
+            rhmap_get(&map, strings[i]);
+        }
+    });
+
+    B_TIME(RhMap, "del", {
         for (int i = 0; i < n; i++) {
             rhmap_del(&map, strings[i]);
         }
-        B_END;
-        B_PRINT("RhMap", "del");
-    }
+    });
 
     rhmap_deinit(&map);
 }
@@ -111,40 +108,49 @@ void benchmark_khash(int n) {
     khash_t(m32) *h = kh_init(m32);
     int absent;
 
-    {
-        B_START;
+    B_TIME(khash_t, "ins", {
         for (int i = 0; i < n; i++) {
             khiter_t iter = kh_put(m32, h, strings[i], &absent);
             kh_value(h, iter) = i;
         }
-        B_END;
-        B_PRINT("khash_t", "ins");
-    }
+    });
 
-    {
-        B_START;
+    B_TIME(khash_t, "get", {
+        for (int i = 0; i < n; i++) {
+            khiter_t iter = kh_get(m32, h, strings[i]);
+            kh_value(h, iter);
+        }
+    });
+
+    B_TIME(khash_t, "del", {
         for (int i = 0; i < n; i++) {
             khiter_t iter = kh_get(m32, h, strings[i]);
             kh_del(m32, h, iter);
         }
-        B_END;
-        B_PRINT("khash_t", "del");
-    }
+    });
+
+    kh_destroy(m32, h);
 }
 
 void benchmark_htab(int n) {
     struct hsearch_data* htab = calloc(1, sizeof(struct hsearch_data));
     hcreate_r(n, htab);
 
-    B_START;
-    for (int i = 0; i < n; i++) {
-        ENTRY entry, *res;
-        entry.key = strings[i];
-        entry.data = i;
-        hsearch_r(entry, ENTER, &res, htab);
-    }
-    B_END;
-    B_PRINT("hsearch", "ins");
+    ENTRY entry, *res;
+    B_TIME(hsearch, "ins", {
+        for (int i = 0; i < n; i++) {
+            entry.key = strings[i];
+            entry.data = i;
+            hsearch_r(entry, ENTER, &res, htab);
+        }
+    });
+
+    B_TIME(hsearch, "get", {
+        for (int i = 0; i < n; i++) {
+            entry.key = strings[i];
+            hsearch_r(entry, FIND, &res, htab);
+        }
+    });
 
     hdestroy_r(htab);
 }
@@ -152,24 +158,23 @@ void benchmark_htab(int n) {
 void benchmark_ghashmap(int n) {
     GHashTable* map = g_hash_table_new(g_str_hash, g_str_equal);
 
-    {
-        B_START;
+    B_TIME(GHashTable, "ins", {
         for (int i = 0; i < n; i++) {
             g_hash_table_insert(map, strings[i], INT_TO_PTR(i));
         }
-        B_END;
-        B_PRINT("GHashTable", "ins");
-    }
+    });
 
-    {
-        B_START;
+    B_TIME(GHashTable, "get", {
+        for (int i =0 ; i < n; i++) {
+            g_hash_table_lookup(map, strings[i]);
+        }
+    });
+
+    B_TIME(GHashTable, "del", {
         for (int i = 0; i < n; i++) {
             g_hash_table_remove(map, strings[i]);
         }
-        B_END;
-        B_PRINT("GHashTable", "del");
-    }
-
+    });
 
     g_hash_table_destroy(map);
 }
@@ -199,33 +204,33 @@ void benchmark_cc_hashtable(int n) {
     CC_HashTable* table;
     cc_hashtable_new(&table);
 
-    {
-        B_START;
+    B_TIME(CC_HashTable, "ins", {
         for (int i =0; i<n;i++)
             cc_hashtable_add(table, strings[i], i);
-        B_END;
-        B_PRINT("CC_HashTable", "ins");
-    }
+    });
 
-    {
-        B_START;
+    B_TIME(CC_HashTable, "get", {
+        for (int i =0 ; i < n; i++) {
+            void* val;
+            cc_hashtable_get(table, strings[i], &val);
+        }
+    });
+
+    B_TIME(CC_HashTable, "del", {
         for (int i = 0; i<n; i++) {
             cc_hashtable_remove(table, strings[i], NULL);
         }
-        B_END;
-        B_PRINT("CC_HashTable", "del");
-    }
+    });
 }
 
 void benchmark_cc_array(int n) {
     CC_Array* arr;
     cc_array_new(&arr);
 
-    B_START;
-    for (int i = 0; i < n; i++)
-        cc_array_add(arr, strings[i]);
-    B_END;
-    B_PRINT("CC_Array", "add");
+    B_TIME(CC_Array, "append", {
+        for (int i = 0; i < n; i++)
+            cc_array_add(arr, strings[i]);
+    });
 }
 
 // --- Benchmarking Memory Funcs --- //
